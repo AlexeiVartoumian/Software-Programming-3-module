3d0faff7d6796835d0a82cb889b1419e199fc1c2
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-02-21 16:48:02 +0000

    Added stubs for bridge pattern

--- src/adapter/XpayToPayDAdapter.java
+++ src/adapter/XpayToPayDAdapter.java
@@ -1,56 +1,64 @@
 package adapter;
 
 public class XpayToPayDAdapter implements PayD {
+  private Xpay xpay;
+
   public XpayToPayDAdapter(Xpay xpay) {
+    this.xpay = xpay;
   }
 
   @Override
   public String getCustCardNo() {
-    return null;
+
+    return xpay.getCreditCardNo();
   }
 
   @Override
   public String getCardOwnerName() {
-    return null;
+    return xpay.getCustomerName();
   }
 
   @Override
   public String getCardExpMonthDate() {
-    return null;
+    return xpay.getCardExpMonth();
   }
 
   @Override
   public Integer getCVVNo() {
-    return null;
+     int converted = (int) xpay.getCardCVVNo();
+     return converted;
   }
 
   @Override
   public Double getTotalAmount() {
-    return null;
+
+    return xpay.getAmount();
   }
 
   @Override
   public void setCustCardNo(String custCardNo) {
-
+    xpay.setCreditCardNo(custCardNo);
   }
 
   @Override
   public void setCardOwnerName(String cardOwnerName) {
-
+    xpay.setCustomerName(cardOwnerName);
   }
 
   @Override
   public void setCardExpMonthDate(String cardExpMonthDate) {
-
+    xpay.setCardExpMonth(cardExpMonthDate);
   }
 
   @Override
   public void setCVVNo(Integer cVVNo) {
 
+    short res =  cVVNo.shortValue();
+    xpay.setCardCVVNo(res);
   }
 
   @Override
   public void setTotalAmount(Double totalAmount) {
-
+    xpay.setAmount(totalAmount);
   }
 }
