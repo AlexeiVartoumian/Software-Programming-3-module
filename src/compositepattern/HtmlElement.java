a89162d70dc880a9694898347f1204ec03affe90
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-02-22 16:42:31 +0000

    Composite Html Structure pattern

--- src/compositepattern/HtmlElement.java
+++ src/compositepattern/HtmlElement.java
@@ -1,6 +1,15 @@
 package compositepattern;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HtmlElement extends HtmlTag {
+  private String tagName;
+  private String startTag;
+
+  private String tagBody;
+  private String endTag;
+  private List<HtmlTag> childElements= new ArrayList<HtmlTag>();
   public HtmlElement(String s) {
     super();
   }
@@ -12,16 +21,34 @@ public class HtmlElement extends HtmlTag {
 
   @Override
   public void setStartTag(String tag) {
-
+    this.startTag = tag;
   }
 
   @Override
   public void setEndTag(String tag) {
-
+    this.endTag = tag;
   }
 
   @Override
   public void generateHtml() {
+    System.out.print(this.startTag);
+    if(this.tagBody!= null){
+      System.out.print(this.tagBody);
+    }
+    System.out.println(this.endTag);
+    for (HtmlTag childElement : childElements) {
+      childElement.generateHtml();
+    }
 
+  }
+  @Override
+  public void addChildTag(HtmlTag htmlTag){
+
+    childElements.add(htmlTag);
+  }
+
+  @Override
+  public void setTagBody(String tagBody){
+    this.tagBody = tagBody;
   }
 }
--- src/compositepattern/HtmlParentElement.java
+++ src/compositepattern/HtmlParentElement.java
@@ -1,6 +1,16 @@
 package compositepattern;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HtmlParentElement extends HtmlTag {
+
+  private String tagName;
+  private String startTag;
+
+  private String tagBody;
+  private String endTag;
+  private List<HtmlTag> childElements = new ArrayList<HtmlTag>();
   public HtmlParentElement(String s) {
     super();
   }
@@ -12,16 +22,36 @@ public class HtmlParentElement extends HtmlTag {
 
   @Override
   public void setStartTag(String tag) {
-
+    this.startTag = tag;
   }
 
   @Override
   public void setEndTag(String tag) {
-
+    this.endTag = tag;
   }
 
   @Override
   public void generateHtml() {
-
+    System.out.println(this.startTag);
+      for (HtmlTag childElement : childElements) {
+          childElement.generateHtml();
+      }
+    if(this.tagBody != null){
+      System.out.println(this.tagBody);
+    }
+    System.out.println(this.endTag);
   }
+
+  @Override
+  public void addChildTag(HtmlTag htmlTag){
+
+    childElements.add(htmlTag);
+  }
+
+  @Override
+  public void setTagBody(String tagBody){
+    this.tagBody = tagBody;
+  }
+
+
 }
